7dc71b2a17dd1b1db04ce4104e6fc01ac7deb7bd
Antonia Ito <ito.antonia@waterdine.studio>
2021-06-17 21:38:17 +0100

    Add extendable scene types.

---
+++ Sources/Flat47StoryGame/RandomChoice.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by x414e54 on 17/06/2021.
+//
+
+import Foundation
+
+
+/*switch
+case "RandomChoice":
+	let variableName: String = sceneData?["VariableToSet"] as! String
+	let possibleFlags: [String] = sceneData?["Flags"] as! [String]
+	let variableTextLookups: [String] = sceneData?["VariableText"] as! [String]
+	let index = Int.random(in: 0 ... possibleFlags.count - 1)
+	
+	let variableText = Bundle.main.localizedString(forKey: variableTextLookups[index], value: nil, table: getChapterTable())
+	variables[variableName] = variableText
+	flags.append(possibleFlags[index])
+	
+	setScene(index: self.currentSceneIndex! + 1)
+	return
+}*/
