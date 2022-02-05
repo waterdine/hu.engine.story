3fc1c5a4676267498979b604abf8512e783da54d
Antonia Ito <ito.antonia@waterdine.studio>
2022-02-05 17:56:51 +0000

    Update info.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "虎.engine.story",
-            dependencies: ["虎.engine.base"]),
+            dependencies: [.product(name: "虎.engine.base", package: "hu-.engine.base")]),
         .testTarget(
             name: "虎.engine.story.tests",
             dependencies: ["虎.engine.story"]),
