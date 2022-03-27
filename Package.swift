dd6a97e7407c129dd239b8ef41b08083eb9e152c
Antonia Ito <ito.antonia@waterdine.studio>
2022-03-27 21:16:01 +0100

    Update for Release 1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/waterdine/hu.engine.base.git", .branch("main"))
+        .package(url: "https://github.com/waterdine/hu.engine.base.git", .upToNextMajor(from: "0.0.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
